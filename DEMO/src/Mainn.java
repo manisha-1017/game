9d2debf9c0ef25cc74f0b76edb950aebd96449c6
Riya <Riya@DESKTOP-95G6PUG>
2024-09-11 00:00:22 +0530

    third commit

--- DEMO/src/Mainn.java
+++ DEMO/src/Mainn.java
@@ -2,6 +2,7 @@
 public class Mainn {
 	public static void main(String[] args) {
 		System.out.println("TRAFFIC");
-		System.out.println("RIDDER");
+		System.out.println("RIDDER..");
+		System.out.println("update1.0");
 	}
 }
